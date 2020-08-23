d93412c780d9debd244de651c1208bb8f1ec3f5b
Devin Bidwell <dbidwell@intuituscyber.com>
2020-08-22 18:02:38 -0700

    fixed things and added comments

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, RouteProps } from "react-router-dom";
 
 /**
  * @param {string} redirectPath A path to redirect the user to (optional) if they are not authenticated
  * @summary Will either 1: Allow passage if user is authenticated or 2: Redirect somewhere safe if they are not
  */
-export default ({ redirectPath, ...props }) => {
+
+interface iPrivateRouteProps extends RouteProps {
+  redirectPath?: string;
+}
+
+export default (props: iPrivateRouteProps) => {
+  const { redirectPath, ...rest } = props;
+
   const token = localStorage.getItem("token");
   return (
     <>
       {token !== undefined && token !== null ? (
-        <Route {...props} />
+        <Route {...rest} />
       ) : (
         <Redirect to={redirectPath || "/login"} />
       )}
