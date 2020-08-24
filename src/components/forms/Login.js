7d46d94cb073216c8011a6b3c92ec9bc7266d00a
EmilyA <emilyadams.3d@gmail.com>
2020-08-24 09:22:44 -0700

    Adding the two form components

---
+++ src/components/forms/Login.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import styled from "styled-components";
+
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 8rem;
+`;
+
+function Login() {
+  return (
+    <div>
+        <Container>
+
+        </Container>
+    </div> 
+    
+  );
+}
+
+export default Login;
